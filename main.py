bbdc101249a696799ddbbe34e86921780a61fa8c
frewtq-art <frewtq@gmail.com>
2026-03-01 22:14:47 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,15 @@
-import os 
-TOKEN = os.getenv("8782328242:AAH5gW4CuLhsUdTb4Bmaq7568nTKZdcaMgw")
-import requests
+import os
 import feedparser
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
-TOKEN = os.environ.get("8782328242:AAH5gW4CuLhsUdTb4Bmaq7568nTKZdcaMgw")
+# Берём токен из Railway Variables
+TOKEN = os.getenv( "8782328242:AAH5gW4CuLhsUdTb4Bmaq7568nTKZdcaMgw")
 
 RSS_FEEDS = {
-    "argentina": "https://eleconomista.com.ar/ultimas-noticias/feed/",
-    "RadioFarda": "https://www.en.radiofarda.com/rssfeeds",
-    "Mehr News" : "http://www.mehrnews.com/rss"
+    "Argentina": "https://eleconomista.com.ar/ultimas-noticias/feed/",
+    "Radio Farda": "https://en.radiofarda.com/rssfeeds",
+    "Mehr News": "https://en.mehrnews.com/rss"
 }
 
 def get_news(feed_url):
@@ -21,10 +20,16 @@ def get_news(feed_url):
     return news_text
 
 async def news(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    argentina_news = get_news(RSS_FEEDS["argentina"])
-    iran_news = get_news(RSS_FEEDS["Iran"])
+    argentina_news = get_news(RSS_FEEDS["Argentina"])
+    farda_news = get_news(RSS_FEEDS["Radio Farda"])
+    mehr_news = get_news(RSS_FEEDS["Mehr News"])
 
-    message = f"🇦🇷 Новости Аргентины:\n{argentina_news}\n\n🇲🇽 Новости ирана:\n{iran_news}"
+    message = (
+        f"🇦🇷 Новости Аргентины:\n{argentina_news}\n\n"
+        f"🇮🇷 Radio Farda:\n{farda_news}\n\n"
+        f"🇮🇷 Mehr News:\n{mehr_news}"
+    )
+
     await update.message.reply_text(message)
 
 app = ApplicationBuilder().token(TOKEN).build()
